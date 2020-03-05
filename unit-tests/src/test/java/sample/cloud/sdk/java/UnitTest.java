4d2d94e207bf22278d2061ffb4b52ef626de7579
Ubuntu Aliaksei Dunets <dualbr@mail.ru>
2020-03-05 17:02:33 +0300

    Initial Commit

---
+++ unit-tests/src/test/java/sample/cloud/sdk/java/UnitTest.java
@@ -0,0 +1,12 @@
+package sample.cloud.sdk.java;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class UnitTest {
+    @Test
+    public void test() {
+        assertTrue(true);
+    }
+}
